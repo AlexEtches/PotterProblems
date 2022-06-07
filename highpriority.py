e7cf6b61daffc0c92613aa09a41834894b6cb387
Alex <alexetches@hotmail.co.uk>
2022-06-07 18:37:13 +0100

    created basic message link to slack

---
+++ highpriority.py
@@ -0,0 +1,32 @@
+import logging
+import os
+import requests
+from azure.servicebus import ServiceBusClient, ServiceBusMessage
+from slack import WebClient
+from slack.errors import SlackApiError
+
+# WebClient instantiates a client that can call API methods
+# When using Bolt, you can use either `app.client` or the `client` passed to listeners.
+client = WebClient(token=os.environ.get("SLACK_BOT_TOKEN"))
+logger = logging.getLogger(__name__)
+# ID of the channel you want to send the message to
+channel_id = "C03KA7PSH88"
+
+CONNECTION_STR = "Endpoint=sb://service-bus-1704.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=BTEYPRWJwh/okUp12fNpRhf0ccoZAVYRX1IKLte4Fbo="
+QUEUE_NAME = "highpriority"
+
+servicebus_client = ServiceBusClient.from_connection_string(conn_str=CONNECTION_STR, logging_enable=True)
+
+def send_slack_message(message):
+    payload = '{"text":"%s"}' %message
+    response = requests.post('https://hooks.slack.com/services/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',
+                              data = payload)
+    print(response.text)
+
+with servicebus_client:
+    receiver = servicebus_client.get_queue_receiver(queue_name=QUEUE_NAME, max_wait_time=5)
+    with receiver:
+        for msg in receiver:
+            print("Received: " + str(msg))
+            send_slack_message(msg)
+            receiver.complete_message(msg)
